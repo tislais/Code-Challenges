d1be02f2f66e3e3b1f2d1bcb95b4ce6f7231e4dc
Tis Lais <tistististis@gmail.com>
2021-05-10 19:51:32 -0700

    add passing returnTen function

--- src/code-challenge-11.js
+++ src/code-challenge-11.js
@@ -1,5 +1,5 @@
 export function returnTen(str) {
-    
+  return str.slice(str.length - 10, str.length).split('');
 }
 
 export function findMax(matrix) {
@@ -11,15 +11,6 @@ export function totalSum(matrix) {
 }
 
 export function grandTotal(stores) {
-  const hoursOpen = ['9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
-
-  const firstPike = [17, 18, 23, 24, 24, 12, 13, 27, 30, 20, 24, 18];
-  const seaTac = [26, 5, 5, 59, 23, 39, 38, 20, 30, 7, 59, 43];
-  const seattleCenter = [7, 14, 19, 22, 15, 4, 23, 27, 28, 23, 1, 29];
-  const capHill = [5, 85, 58, 51, 50, 13, 33, 32, 47, 94, 31, 62];
-  const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
-
-  const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 }
 
