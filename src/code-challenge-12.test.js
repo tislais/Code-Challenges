488419ef7eb245fe3981b77bda4a1bf8e4217e3a
Tis Lais <tistististis@gmail.com>
2021-05-11 23:15:11 -0700

    add passing addValues function and test

--- src/code-challenge-12.test.js
+++ src/code-challenge-12.test.js
@@ -1,4 +1,4 @@
-import { toLastNames } from './code-challenge-12.js';
+import { toLastNames, addValues } from './code-challenge-12.js';
 
 test('should iterate over an array of people objects and creates a new list of each persons full name', () => {
   const people = [
@@ -9,4 +9,9 @@ test('should iterate over an array of people objects and creates a new list of e
   expect(toLastNames(people)).toEqual(names);
 });
 
+test('should add numbers in an array', () => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const sum = 55;
+  expect(addValues(numbers)).toEqual(sum);
+});
 
