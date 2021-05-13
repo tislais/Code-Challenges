e8ee90a87d00f0b2136675447ba0ccbc715ed416
Tis Lais <tistististis@gmail.com>
2021-05-13 08:50:31 -0700

    initial commit

---
+++ src/code-challenge-13.js
@@ -0,0 +1,44 @@
+
+export const sortByChildren = (charArray) => {
+ 
+};
+
+export const containsW = (str) => {
+
+};
+
+export const isNum = (input) => {
+  
+};
+
+export const containsWorld = (input) => {
+
+};
+
+export const isCapitalized = (str) => {
+
+};
+
+export const citiesAtoJ = (arr) => {
+
+};
+
+// stretch 
+
+export const matchMonth = (input) => {
+
+};
+
+export const noPunctuation = str => {
+
+};
+
+export let hangman = (str) => {
+
+};
+
+const seashells = `She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I'm sure she sells seashore shells.`;
+
+export const findShells = (str) => {
+
+};
---
+++ src/code-challenge-13.test.js
@@ -0,0 +1,8 @@
+
+
+// describe('Testing challenge 1', () => {
+//   test('It should sort the characters by number of children', () => {
+//     expect(sortByChildren(characters)[0].name).toStrictEqual('Euron');
+//     expect(sortByChildren(characters)[0].children.length).toStrictEqual(0);
+//   });
+// });
